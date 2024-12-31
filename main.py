d7d2ca4609a9bf53d9265a4538836faf62e4d882
Shetty Dhiraj Anand <88022432+SHETTY-DHIRAJ@users.noreply.github.com>
2024-12-31 22:11:37 +0530

    Updating main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 # load environment variables
 load_dotenv()
 
-# Note: For ease of work one can try mentioning all the respective values for below variables in string.
+# Note: Please make sure to set the following environment variables in your .env file
 api_key= os.getenv("azure_openai_api_key") # Here you have to mention your azure openai api key.
 api_version= os.getenv("azure_api_version") # Here you have to mention your azure openai api version
 azure_endpoint= os.getenv("azure_endpoint") # Here you have to mention your azure openai endpoint
