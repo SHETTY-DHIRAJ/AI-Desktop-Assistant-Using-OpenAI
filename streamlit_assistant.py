72c0926592c33fb0e2c5dc862e5cb0afd81630d4
Shetty Dhiraj Anand <88022432+SHETTY-DHIRAJ@users.noreply.github.com>
2025-01-05 17:15:28 +0530

    Importing all the required libraries in streamlit_assistant.py

---
+++ streamlit_assistant.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+import pyttsx3
+import subprocess
+import os
+import webbrowser
+import requests
+import datetime
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+import streamlit as st
