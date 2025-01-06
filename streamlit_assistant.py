ed9fe521ab6c37d4b53d240ed922ab3a785fa258
Shetty Dhiraj Anand <88022432+SHETTY-DHIRAJ@users.noreply.github.com>
2025-01-06 20:08:00 +0530

    Updating streamlit_assistant.py with the environment variable.

--- streamlit_assistant.py
+++ streamlit_assistant.py
@@ -8,3 +8,12 @@ import datetime
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 import streamlit as st
+
+# load environment variables
+load_dotenv()
+
+# Note: For ease of work one can try mentioning all the respective values for below variables in string.
+api_key= os.getenv("azure_openai_api_key") # Here you have to mention your azure openai api key.
+api_version= os.getenv("azure_api_version") # Here you have to mention your azure openai api version.
+azure_endpoint= os.getenv("azure_endpoint") # Here you have to mention your azure openai endpoint.
+azure_deployment= os.getenv("azure_deployment_name") # Here you have to mention your azure openai deployment name.
